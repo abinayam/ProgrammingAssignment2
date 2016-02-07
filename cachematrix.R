37273ea497fb3af9192e406a6179b585c0c1ed13
Abinaya Mahendiran <abinayamahendiran@yahoo.com>
2016-02-07 21:50:52 +0530

    Programming Assignment - 2 : Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creates a special “matrix” object that can cache its inverse, 
+## which is really a list containing a function to
+## 1. set() -> sets the matrix
+## 2. get() -> gets the matrix
+## 3. setinverse() -> sets the inverse of the matrix
+## 4. getinverse() -> gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+        matrix_inverse <- NULL
+        
+        ## Set the matrix
+        set <- function(y) {
+          x <<- y
+          matrix_inverse <<- NULL
+        }
+        
+        ## Get the matrix
+        get <- function() x
+        
+        ## Set the inverse of the matrix
+        setinverse <- function(invert_matrix) matrix_inverse <<- invert_matrix
+        
+        ## Get the inverse of the matrix
+        getinverse <- function() matrix_inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+        matrix_inverse <- x$getinverse()
+        
+        ## Check if the inverse is already cached and not changed, 
+        ## if so return the cached inverse
+        if(!is.null(matrix_inverse)) {
+          message("getting cached data")
+          return(matrix_inverse)
+        }
+        
+        ## If the matrix is not cached, calculate the inverse and return
+        matrix_data <- x$get()
+        matrix_inverse <- solve(matrix_data, ...)
+        x$setinverse(matrix_inverse)
+        return(matrix_inverse)
 }
+
